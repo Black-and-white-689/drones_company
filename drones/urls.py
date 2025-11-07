3c4113f3ede825b098f75720779540eb66f600d7
Ihor Pershin <issue.07.08.09@gmail.com>
2025-11-07 17:41:16 +0200

    Added imports and task-urls in urls file

---
+++ drones/urls.py
@@ -0,0 +1,60 @@
+from django.urls import path
+
+from .views import (
+TaskListView,
+TaskDetailView,
+TaskCreateView,
+TaskDeleteView,
+TaskUpdateView,
+WorkerListView,
+WorkerDetailView,
+WorkerCreateView,
+WorkerDeleteView,
+PositionListView,
+PositionCreateView,
+PositionDeleteView,
+PositionUpdateView,
+TaskTypeListView,
+TaskTypeCreateView,
+TaskTypeUpdateView,
+TaskTypeDeleteView,
+toggle_assign_to_task,
+)
+
+
+app_name = "drones"
+
+
+urlpatterns = [
+    #  Tasks
+    path(
+        "tasks/",
+        TaskListView.as_view(),
+        name="task-list"
+    ),
+    path(
+        "tasks/<int:pk>/",
+        TaskDetailView.as_view(),
+        name="task-detail"
+    ),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path(
+        "tasks/<int:pk>/toggle-assign/",
+        toggle_assign_to_task,
+        name="task-toggle-assign"
+    ),
+]
