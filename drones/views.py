536f932420c1f89c60d4dfd3a2a098a49656083a
Ihor Pershin <issue.07.08.09@gmail.com>
2025-11-07 17:04:12 +0200

    Added List, Detail, Create, Update, and Delete by Task to views

--- drones/views.py
+++ drones/views.py
@@ -21,3 +21,48 @@ from .forms import (
     TaskTypeSearchForm,
     WorkerCreationForm,
 )
+
+
+#  Tasks
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    template_name = "drones/task_list.html"
+    context_object_name = "task_list"
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = TaskSearchForm(self.request.GET)
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset().select_related("task_type").prefetch_related("assignees")
+        query = self.request.GET.get("q")
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+        return queryset.order_by("id")
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+    template_name = "drones/task_detail.html"
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("drones:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("drones:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("drones:task-list")
