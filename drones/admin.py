c0dbf90a64890808c8dddef5650c4cf03b52a6d6
Ihor Pershin <issue.07.08.09@gmail.com>
2025-11-07 18:09:18 +0200

    Added TaskInLine in admin file

--- drones/admin.py
+++ drones/admin.py
@@ -3,3 +3,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import Worker, Task, TaskType, Position
+
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 0
+    fields = ("name", "is_done", "priority", "deadline")
+    readonly_fields = ("name", "is_done", "priority", "deadline")
+    show_change_link = True
