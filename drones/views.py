47ab05a7cdb2224c1d3daf4b3e062aae3c0fdfce
Ihor Pershin <issue.07.08.09@gmail.com>
2025-11-07 16:58:56 +0200

    Added imports to views

--- drones/views.py
+++ drones/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse_lazy, reverse
+
+from django.views import generic
+
+from .models import Task, Worker, TaskType, Position
+
+from .forms import (
+    TaskForm,
+    TaskSearchForm,
+    WorkerSearchForm,
+    PositionSearchForm,
+    TaskTypeSearchForm,
+    WorkerCreationForm,
+)
